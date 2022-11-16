7843c4e8195f16d76d3f077ba446f57323a5549f
Zobaer <zzobaer09@gmail.com>
2022-11-16 18:33:52 +0600

    page to create room created. send data to backend.

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,118 @@
 import React ,{ Component } from "react";
-
+import Button from "@material-ui/core/Button"
+import Grid from "@material-ui/core/Grid"; 
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom"
+import  Radio  from "@material-ui/core/Radio";
+import  RadioGroup  from "@material-ui/core/RadioGroup";
+import  FormControlLabel  from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component{
+    defaultVotes = 2;
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        }
+        
+        this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+        this.handleVotesChange = this.handleVotesChange.bind(this);
     }
+
+    handleVotesChange(e){
+        this.setState({
+            votesToSkip : e.target.value,
+        });
+    }
+
+    handleGuestCanPauseChange(e){
+        this.setState({
+            guestCanPause: e.target.value === "true" ? true : false,
+        });
+    }
+
+    handleRoomButtonPressed(){
+        console.log(this.state)
+        const requestOptions = {
+            method : 'POST',
+            headers : {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                votes_to_skip : this.state.votesToSkip,
+                guest_can_pause : this.state.guestCanPause,
+            })
+        }
+        const url = "/api/create-room/"
+        fetch(url , requestOptions).then(response => response.json()).then(data => console.log(data))
+    }
+
     render(){
-        return <h1>Create New Room</h1>
+
+        return (
+            <Grid container spacing={1}>
+
+                <Grid item align="center" xs={12}>
+                    <Typography component="h4" variant="h4">
+                        Create Room
+                    </Typography>
+                </Grid>
+                
+                <Grid item align="center" xs={12}>
+                    <FormControl component="fieldset">
+
+                        <FormHelperText>
+                            <div align="center">
+                                <p>
+                                    Guest Control of playback satate
+                                </p>
+                            </div>
+                        </FormHelperText>
+
+                        <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
+                            <FormControlLabel value="true" control={<Radio color="primary"/>} label="Play/Pause" labelPlacement="bottom"/>
+                            <FormControlLabel value="false" control={<Radio color="secondary"/>} label="no Control" labelPlacement="bottom"/>
+                        </RadioGroup>
+
+                    </FormControl>
+                </Grid>
+
+                <Grid item align="center" xs={12}>
+                    <FormControl>
+                        <TextField 
+                        required={true} 
+                        type="number" 
+                        defaultValue={this.defaultVotes} 
+                        onChange={this.handleVotesChange}
+                        inputProps={{
+                            min:1,
+                            style : {textAlign:"center"}
+                            }}/>
+
+                        <FormHelperText>
+                            <div align="center">Votes required to skip</div>
+                        </FormHelperText>
+
+                    </FormControl>
+                </Grid>
+                
+                <Grid item align="center" xs={12}>
+                    <Button color="primary" variant="contained" onClick={this.handleRoomButtonPressed}>
+                        Create a room
+                    </Button>
+                </Grid>
+
+                <Grid item align="center" xs={12}>
+                    <Button color="secondary" variant="contained" to="/" component={Link}>
+                        Back
+                    </Button>
+                </Grid>
+            </Grid>
+            
+        )
     }
 }
