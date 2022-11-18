e2ac1fa897c7fb19879f9ac84b683a1473442977
Zobaer <zzobaer09@gmail.com>
2022-11-18 20:32:56 +0600

    join page created

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,63 @@
 import React,{ Component } from "react";
-
-
+import {TextField , Button , Grid , Typography} from "@material-ui/core"
+import { Link } from "react-router-dom"
 export default class RoomJoinPage extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            error : "",
+            roomCode : ""
+        }
+        this._handleTextFieldChange = this._handleTextFieldChange.bind(this)
+        this._roomButtonPressed = this._roomButtonPressed.bind(this)
+
     }
     render(){
-        return <h1>Join In a room</h1>
+        return (
+            <Grid container spacing={1} alignItems="center">
+                
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4">
+                        Join a Room
+                    </Typography>
+                </Grid>
+                
+                <Grid item xs={12} align="center">
+                    <TextField 
+                        error={this.state.error}
+                        label="Code"
+                        placeholder="enter room code"
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant="outlined"
+                        onChange={this._handleTextFieldChange}
+                    />
+                </Grid>
+
+                {/* buttons */}
+                <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained" onClick={this._roomButtonPressed}>
+                        Join Room
+                    </Button>
+                </Grid>
+
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" to="/" component={ Link }>
+                        Back
+                    </Button>
+                </Grid>
+                {/* button end */}
+            </Grid>
+            
+        )
+    }
+
+    _handleTextFieldChange(e){
+        this.setState({
+            roomCode: e.target.value
+        })
+    }
+    _roomButtonPressed(){
+        console.log(this.state.roomCode);
     }
 }
